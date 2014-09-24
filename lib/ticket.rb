634106668b86a89a5b05123be53a9c3bcf7d0837
Felix Wetell <felix.wetell@elev.it-gymnasiet.se>
2014-09-24 13:42:40 +0200

    Changed all "and" to "&&"

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -5,9 +5,9 @@ def ask_age()
 end
 
 def ticket_price(age)
-  if age >= 0 and age <= 17
+  if age >= 0 && age <= 17
     ticket = 10
-  elsif age >= 18 and age <= 64
+  elsif age >= 18 && age <= 64
     ticket = 20
   else age >= 65
     ticket = 15
