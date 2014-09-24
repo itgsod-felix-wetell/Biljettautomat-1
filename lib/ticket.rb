4cabf4dfac9bd8a4cd5586770e9b6ebc85a1df62
Felix Wetell <felix.wetell@elev.it-gymnasiet.se>
2014-09-24 13:40:11 +0200

    Price for everyone works

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -7,8 +7,10 @@ end
 def ticket_price(age)
   if age >= 0 and age <= 17
     ticket = 10
-  else age >= 18 and age <= 64
+  elsif age >= 18 and age <= 64
     ticket = 20
+  else age >= 65
+    ticket = 15
   end
   return ticket
 end
